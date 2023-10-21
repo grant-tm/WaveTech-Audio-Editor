f474fecdaa0e1b0331d918614a47b22ec06c1786
grant-tm <grant.t.morgan@gmail.com>
2023-10-20 17:12:50 -0700

    expand wavefile object definition

--- wavefile header/wavefile.c
+++ wavefile header/wavefile.c
@@ -1,4 +1,22 @@
-#ifndef WAVELIB.H
-#define WAVELIB.H
+#include "wavefile.h"
 
-#endif
+WaveFile* wavefile_create(){
+
+    WaveFile* new_wavefile = malloc(sizeof(WaveFile));
+    
+    /* initialize values */
+    
+    return new_wavefile;
+}
+
+WaveFile* wavefile_unpack_data(WaveFile* wavefile, int* data);
+
+WaveFile* wavefile_pack_data(WaveFile* wavefile, int* data);
+
+void* wavefile_destroy(WaveFile* file_to_destroy){
+
+    /* free data memory */
+    /* free file_to_destroy */
+
+    return file_to_destroy;
+}
