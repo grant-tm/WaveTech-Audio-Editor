a969da035630c47632d5293211563cb42cfc0716
grant-tm <grant.t.morgan@gmail.com>
2023-11-10 08:48:38 -0800

    set up reverse audio test

--- source/gui.cpp
+++ source/gui.cpp
@@ -28,32 +28,34 @@
 =====================================================================================================================*/
 int main(int argc, char** argv){
     
-    /* client-server setup */
+    // client-server setup
     asio::io_context io_context;
-    /* resolve endpoint */
+    // resolve endpoint
     asio::ip::tcp::resolver resolver(io_context);
     asio::ip::tcp::resolver::results_type endpoint = resolver.resolve("localhost", "daytime");
-    /* open socket and connect to endpoint */
+    // open socket and connect to endpoint
     std::shared_ptr<asio::ip::tcp::socket> socket_ptr(new asio::ip::tcp::socket(io_context));
     asio::connect(*socket_ptr, endpoint);
 
-    /* load a file */
+    //load a file 
     AudioFile<float> file;
     file.load("../wav_files/sin_sweep.wav");
     file.printSummary();
-    /* create new audio buffer */
+    
+    // send command to dsp
+    send_message(socket_ptr, "reverse");
+    
+    // send audio to dsp
+    send_audio(socket_ptr, &(file.samples));
+    
+    // receive audio from dsp
     AudioFile<float>::AudioBuffer buf;
     buf.resize(2);
-
-    /* send audio to dsp */
-    bool result = send_audio(socket_ptr, &(file.samples));
-    /* receive audio from dsp */
     recv_audio(socket_ptr, &buf);
-
-    /* put new audio in opened file and save as result.wav */
+    
+    //put new audio in opened file and save as result.wav 
     file.setAudioBuffer(buf);
     file.printSummary();
     file.save("../wav_files/result.wav");
-    
     return 0;
 }
