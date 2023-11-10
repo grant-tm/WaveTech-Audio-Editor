fa4f1def239adb35cecb917a0230c993aba73346
grant-tm <grant.t.morgan@gmail.com>
2023-11-10 08:47:36 -0800

    add reverse method

--- source/dsp.cpp
+++ source/dsp.cpp
@@ -22,26 +22,74 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <vector>
+/*=====================================================================================================================
+== Pitch Shift
+=====================================================================================================================*/
+void pitch_shift(std::shared_ptr<asio::ip::tcp::socket> socket){
+    
+}
+/*=====================================================================================================================
+== Time Stretch
+=====================================================================================================================*/
+void time_stretch(std::shared_ptr<asio::ip::tcp::socket> socket){
 
+}
+/*=====================================================================================================================
+== Reverse
+=====================================================================================================================*/
+void reverse(std::shared_ptr<asio::ip::tcp::socket> socket){
+    
+    AudioFile<float>::AudioBuffer buffer; 
+    buffer.resize(2);
+    
+    recv_audio(socket, &buffer);
+
+    //for each audio channel
+    for(int i=0; i<buffer.size(); i++){
+        size_t num_samples = buffer[i].size();
+        // for each sample in an audio channel
+        for(int j=0; j<(num_samples - j - 1); j++){
+            // swap two samples
+            float temp = buffer[i][j];
+            buffer[i][j] = buffer[i][num_samples - j - 1];
+            buffer[i][num_samples - j - 1] = temp;
+        }
+    }
+
+    send_audio(socket, &buffer);
+}
 /*=====================================================================================================================
 == Main
 =====================================================================================================================*/
 int main(int argc, char** argv){
 
-    /* initialize context and socket */
+    // initialize context and socket
     asio::io_context io_context;
     std::shared_ptr<asio::ip::tcp::socket> socket_ptr(new asio::ip::tcp::socket(io_context));
     
-    /* acceptor */
+    // acceptor
     std::shared_ptr<asio::ip::tcp::acceptor> acceptor_ptr
         (new asio::ip::tcp::acceptor(io_context, asio::ip::tcp::endpoint(asio::ip::tcp::v4(), 13)));
     (*acceptor_ptr).accept(*socket_ptr);
 
-    AudioFile<float>::AudioBuffer new_buffer; 
-    new_buffer.resize(2);
-    
-    recv_audio(socket_ptr, &new_buffer);
-    send_audio(socket_ptr, &new_buffer);
+    while(1){
+        std::string receiver;
+        recv_message(socket_ptr, &receiver);
+
+        if(receiver.compare("pitchshift") == 0){
+            pitch_shift(socket_ptr);
+        }
+        else if(receiver.compare("timestretch") == 0){
+            time_stretch(socket_ptr);
+        }
+        else if(receiver.compare("reverse") == 0){
+            reverse(socket_ptr);
+            return 0;
+        }
+        else if(receiver.compare("exit") == 0){
+            return 0;
+        }
+    }
 
     return 0;
 }
