a5bd7346a4eff4e1d966b78a648527c72b75d215
grant-tm <grant.t.morgan@gmail.com>
2023-11-10 08:48:06 -0800

    recv_message protocol resizes container

--- source/communication_protocols.hpp
+++ source/communication_protocols.hpp
@@ -53,8 +53,10 @@ unsigned int recv_message(std::shared_ptr<asio::ip::tcp::socket> socket, std::st
     
     // receive characters
     size_t read_size = (*socket).read_some(asio::buffer(receiver));
-    
+   
+
     // copy message to container
+    receiver.resize(read_size);
     *container = receiver;
     
     // return read size
@@ -72,12 +74,12 @@ bool send_audio(std::shared_ptr<asio::ip::tcp::socket> socket,
     asio::error_code ignored_error;
     std::vector<float> chunked_samples;
 
-    /* send left channel data */
+    // send left channel data
     chunked_samples.resize(CHUNK_SIZE);
     unsigned int buffer_idx = 0;
     size_t buffer_size = (*samples)[0].size();
     while(1){
-        /* break samples into chunks */
+        // break samples into chunks
         for(unsigned int i=0; i<CHUNK_SIZE; i++){
             chunked_samples[i] = (*samples)[0][buffer_idx++];
             if(buffer_idx >= buffer_size){
@@ -85,27 +87,30 @@ bool send_audio(std::shared_ptr<asio::ip::tcp::socket> socket,
                 break;
             }
         }
-        /* write chunks to socket */
+        // write chunks to socket
         size_t write_size = chunked_samples.size() * sizeof(float);
         write_size = asio::write(*socket, asio::buffer(chunked_samples, write_size), ignored_error);
         
-        /* break when less than a full chunk is used */
+        // break when less than a full chunk is used
         unsigned int num_samples_written = static_cast<unsigned int>(write_size/sizeof(float));
         if(num_samples_written < CHUNK_SIZE){
             break;
         }
     }
     
-    /* wait for confirmation */
+    // wait for confirmation
     std::string msg;
     recv_message(socket, &msg);
+    if(msg.compare("CONFIRMED") != 0){
+        return false;
+    }
 
-    /* send right channel data */
+    // send right channel data
     chunked_samples.resize(CHUNK_SIZE);
     buffer_idx = 0;
     buffer_size = (*samples)[1].size();
     while(1){
-        /* break samples into chunks */
+        // break samples into chunks
         for(unsigned int i=0; i<CHUNK_SIZE; i++){
             chunked_samples[i] = (*samples)[1][buffer_idx++];
             if(buffer_idx >= buffer_size){
@@ -113,10 +118,10 @@ bool send_audio(std::shared_ptr<asio::ip::tcp::socket> socket,
                 break;
             }
         }
-        /* write chunks to socket */
+        // write chunks to socket
         size_t write_size = chunked_samples.size() * sizeof(float);
         write_size = asio::write(*socket, asio::buffer(chunked_samples, write_size), ignored_error);
-        /* break when less than a full chunk is used */
+        // break when less than a full chunk is used
         unsigned int num_samples_written = static_cast<unsigned int>(write_size/sizeof(float));
         if(num_samples_written < CHUNK_SIZE){
             break;
@@ -133,7 +138,7 @@ void recv_audio(std::shared_ptr<asio::ip::tcp::socket> socket,
     
     std::vector<float> recv;
     
-    /* get left channel data */
+    // get left channel data
     recv.resize(CHUNK_SIZE);
     unsigned int buffer_idx = 0;
     while(1){
@@ -151,12 +156,12 @@ void recv_audio(std::shared_ptr<asio::ip::tcp::socket> socket,
         }
     }
     
-    /* send confirmation */
+    // send confirmation
     asio::error_code ignored_error;
     std::string msg = "CONFIRMED";
     send_message(socket, msg);
 
-    /* get right channel data */
+    // get right channel data
     recv.resize(CHUNK_SIZE);
     buffer_idx = 0;
     while(1){
