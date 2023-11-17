87d3bab4bb69d03d038d4f043bbb65c74a266820
grant-tm <grant.t.morgan@gmail.com>
2023-11-16 21:03:36 -0800

    now acts as a multiplexer to DSP services

--- source/dsp.cpp
+++ source/dsp.cpp
@@ -1,6 +1,6 @@
-/*=====================================================================================================================
-== Include Dependencies
-=====================================================================================================================*/
+//=================================================================================================
+// Include Dependencies
+//=================================================================================================
 #include "..\dependencies\AudioFile\AudioFile.h"
 
 //define windows version for asio (suppresses annoying warning)
@@ -15,225 +15,96 @@
 //defines protocols for sending and receiving audio through socket
 #include "communication_protocols.hpp"
 
-/*=====================================================================================================================
-== Standard Libraries
-=====================================================================================================================*/
+//=================================================================================================
+// Standard Libraries
+//=================================================================================================
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <vector>
-
-/*=====================================================================================================================
-== Audio Buffer Copying Functions
-=====================================================================================================================*/
-void buffer_copy_full(AudioFile<float>::AudioBuffer* original, AudioFile<float>::AudioBuffer* copy){
-
-    // get the number of channels in the original buffer
-    size_t num_channels = (*original).size();
-
-    //copy contents of original buffer into copy
-    for(size_t channel=0; channel<num_channels; channel++){
-        //clear the audio vector to be copied into
-        (*copy)[channel].clear();
-        // get the number of samples in the original's channel
-        size_t num_samples = (*original)[channel].size();
-        //resize the copy channel's audio vector
-        (*copy)[channel].resize(num_samples);
-        //copy samples from original buffer into copy
-        for(unsigned int sample=0; sample<num_samples; sample++){
-            (*copy)[channel][sample] = (*original)[channel][sample];
-        }
-    }
-}
-
-void buffer_copy_part(AudioFile<float>::AudioBuffer* buf1, size_t buf1_idx,
-                    AudioFile<float>::AudioBuffer* buf2, size_t buf2_idx, size_t num_samples){
-
-    // get the number of channels in the original buffer
-    size_t num_channels = (*buf1).size();
-    size_t buf1_size = (*buf1)[0].size();
-    size_t buf2_size = (*buf2)[0].size();
-
-    // ensure both buffers are large enough
-    if(buf1_size < (buf1_idx + num_samples)){
-        buf1_size += (buf1_idx + num_samples) - buf1_size;
-        (*buf1)[0].resize(buf1_size);
-        (*buf1)[1].resize(buf1_size);
-    }
-    if(buf2_size < (buf2_idx + num_samples)){
-        buf2_size += (buf2_idx + num_samples) - buf2_size;
-        (*buf2)[0].resize(buf2_size);
-        (*buf2)[1].resize(buf2_size); 
-    }
-
-    //copy contents of buf1 to buf2
-    for(size_t channel=0; channel<num_channels; channel++){
-        for(size_t sample=0; sample<num_samples; sample++){
-            (*buf2)[channel][buf2_idx + sample] = (*buf1)[channel][buf1_idx + sample];
-        }
-    }
-}
-
-/*=====================================================================================================================
-== Functions Relevant to SOLA (Synchronous Overlap Add)
-=====================================================================================================================*/
-void window_buffers(AudioFile<float>::AudioBuffer* buf1, AudioFile<float>::AudioBuffer* buf2){
+//=================================================================================================
+// Connect to reverse microservice
+//=================================================================================================
+void reverse(asio::io_context& io_context, AudioFile<float>::AudioBuffer* buf)
+{
+    // connect to reverse microservice
+    std::shared_ptr<asio::ip::tcp::socket> reverse_connection_socket(new asio::ip::tcp::socket(io_context));
+    asio::ip::tcp::resolver resolver(io_context);
+    asio::connect(*reverse_connection_socket, resolver.resolve("localhost", "8800"));
     
-    // get the number of channels in the first buffer
-    size_t num_channels = (*buf1).size();
-    size_t buf1_size = (*buf1)[0].size();
-    size_t buf2_size = (*buf2)[0].size();
+    // send run command and audio over port 8800
+    send_message(reverse_connection_socket, "run");
+    std::string receiver;
+    recv_message(reverse_connection_socket, &receiver);
+    if(receiver.compare("ready") == 0)
+    {
+        send_audio(reverse_connection_socket, buf);
+    }
+    recv_message(reverse_connection_socket, &receiver);
     
-    // ensure buffers are equal length
-    if(buf1_size < buf2_size){
-        buf1_size = buf2_size;
-        (*buf1)[0].resize(buf2_size);
-        (*buf1)[1].resize(buf2_size);
-    }
-    else if(buf2_size < buf1_size){
-        buf2_size = buf1_size;
-        (*buf2)[0].resize(buf1_size);
-        (*buf2)[1].resize(buf1_size);
-    }
-    size_t buf_size = buf1_size;
+    // receive response from reverse
+    send_message(reverse_connection_socket, "ready");
+    recv_audio(reverse_connection_socket, buf);
 
-    //window and combine the temporary buffers
-    for(size_t channel=0; channel<num_channels; channel++){
-        for(size_t sample=0; sample<buf_size; sample++){
-            (*buf1)[channel][sample] *= ((float)(buf_size-sample)/(float)(buf_size)); 
-            (*buf2)[channel][sample] *= ((float)(sample)/(float)(buf_size));
-            (*buf1)[channel][sample] += (*buf2)[channel][sample];
-        }
-    }
-}
-
-void sola(AudioFile<float>::AudioBuffer* input_samples, float time_scale){
-    
-    // independent values
-    unsigned int frame_size = 4000;
-    unsigned int overlap_size = 800;
-    
-    // dependent values
-    unsigned int flat_duration = frame_size - (2 * overlap_size);
-    unsigned int frame_skip = (int)((float)(frame_size - overlap_size) * (float)time_scale);
-
-    // input output
-    size_t num_input_samples = (*input_samples)[0].size();
-    size_t num_channels = input_samples->size();
-    size_t input_cursor = 0;
-
-    // output buffer
-    AudioFile<float>::AudioBuffer output_samples;
-    output_samples.resize(num_channels);
-    size_t output_cursor = 0;
-
-    // divide audio into overlapping frames
-    while(input_cursor <= num_input_samples - 2*(frame_skip + overlap_size)){
-
-        // copy flat section to the output
-        buffer_copy_part(input_samples, input_cursor, &output_samples, output_cursor, flat_duration);
-
-        // move cursors
-        output_cursor += flat_duration;
-        input_cursor += flat_duration;
-        
-        // temporary buffer holds current frame's overlap tail
-        AudioFile<float>::AudioBuffer prev_frame_tail;
-        prev_frame_tail.resize(num_channels);
-        buffer_copy_part(input_samples, input_cursor, &prev_frame_tail, 0, overlap_size);
-        
-        // temporary buffer holds next frame's overlap head
-        AudioFile<float>::AudioBuffer next_frame_head;
-        next_frame_head.resize(num_channels);
-        buffer_copy_part(input_samples, (input_cursor + frame_skip - overlap_size), &next_frame_head, 0, overlap_size);
-
-        // move cursor to next frame's flat section
-        input_cursor += frame_skip;
-
-        // window and combine the overlap buffers
-        window_buffers(&prev_frame_tail, &next_frame_head);
-        
-        // add the overlap to the output buffer
-        buffer_copy_part(&prev_frame_tail, 0, &output_samples, output_cursor, overlap_size);
-        output_cursor += overlap_size;
-    }
-    // copy the full output to the input container and return
-    buffer_copy_full(&output_samples, input_samples);
     return;
 }
 
-/*=====================================================================================================================
-== Pitch Shift
-=====================================================================================================================*/
-void pitch_shift(std::shared_ptr<asio::ip::tcp::socket> socket){
-    
-}
-/*=====================================================================================================================
-== Time Stretch
-=====================================================================================================================*/
-void time_stretch(std::shared_ptr<asio::ip::tcp::socket> socket){
-    AudioFile<float>::AudioBuffer buffer; 
-    buffer.resize(2);
-    
-    recv_audio(socket, &buffer);
-    
-    sola(&buffer, (float)0.5);
-    
-    send_audio(socket, &buffer);
-}
-/*=====================================================================================================================
-== Reverse
-=====================================================================================================================*/
-void reverse(std::shared_ptr<asio::ip::tcp::socket> socket){
-    
-    AudioFile<float>::AudioBuffer buffer; 
-    buffer.resize(2);
-    
-    recv_audio(socket, &buffer);
-
-    //for each audio channel
-    for(int i=0; i<buffer.size(); i++){
-        size_t num_samples = buffer[i].size();
-        // for each sample in an audio channel
-        for(int j=0; j<(num_samples - j - 1); j++){
-            // swap two samples
-            float temp = buffer[i][j];
-            buffer[i][j] = buffer[i][num_samples - j - 1];
-            buffer[i][num_samples - j - 1] = temp;
-        }
-    }
-
-    send_audio(socket, &buffer);
-}
-/*=====================================================================================================================
-== Main
-=====================================================================================================================*/
-int main(int argc, char** argv){
-
-    // initialize context and socket
+//=================================================================================================
+// Main
+//=================================================================================================
+int main(int argc, char** argv)
+{
+    // receive connection from gui
     asio::io_context io_context;
-    std::shared_ptr<asio::ip::tcp::socket> socket_ptr(new asio::ip::tcp::socket(io_context));
-    
-    // acceptor
+    std::shared_ptr<asio::ip::tcp::socket> gui_connection_socket(new asio::ip::tcp::socket(io_context));
     std::shared_ptr<asio::ip::tcp::acceptor> acceptor_ptr
-        (new asio::ip::tcp::acceptor(io_context, asio::ip::tcp::endpoint(asio::ip::tcp::v4(), 13)));
-    (*acceptor_ptr).accept(*socket_ptr);
+        (new asio::ip::tcp::acceptor(io_context, asio::ip::tcp::endpoint(asio::ip::tcp::v4(), 5500)));
+    (*acceptor_ptr).accept(*gui_connection_socket);
 
-    while(1){
+    while(1)
+    {
+        std::cout << "waiting..." << std::endl;
+        
         std::string receiver;
-        recv_message(socket_ptr, &receiver);
+        recv_message(gui_connection_socket, &receiver);
+        std::cout << "Message received: " << receiver << std::endl;
+        if(receiver.compare(0, 5, "shift") == 0)
+        {
+            std::cout << "shift" << std::endl;
+        }
+        else if(receiver.compare(0, 6, "stretch") == 0)
+        {
+            std::cout << "stretch"<< std::endl;
+        }
+        else if(receiver.compare(0, 7, "reverse") == 0)
+        {
+            //create new audio buffer
+            AudioFile<float>::AudioBuffer buf;
+            buf.resize(2);
+            // receive audio from the gui
+            recv_audio(gui_connection_socket, &buf);
+            // send audio to reverse microservice
+            reverse(io_context, &buf);
+            // send reversed audio back to the gui
+            send_audio(gui_connection_socket, &buf);
+        }
+        else if(receiver.compare(0, 4, "exit") == 0){
+            std::cout << "Exiting..." << std::endl;
+            
+            // connect to reverse
+            std::shared_ptr<asio::ip::tcp::socket> reverse_connection_socket(new asio::ip::tcp::socket(io_context));
+            asio::ip::tcp::resolver resolver(io_context);
+            asio::connect(*reverse_connection_socket, resolver.resolve("localhost", "8800"));
+            
+            //send run command and audio over port 8800
+            send_message(reverse_connection_socket, "exit");
+            
+            // close connection
+            gui_connection_socket->shutdown(asio::ip::tcp::socket::shutdown_both);
+            gui_connection_socket->close();
 
-        if(receiver.compare("pitchshift") == 0){
-            pitch_shift(socket_ptr);
-        }
-        else if(receiver.compare("timestretch") == 0){
-            time_stretch(socket_ptr);
-        }
-        else if(receiver.compare("reverse") == 0){
-            reverse(socket_ptr);
-        }
-        else if(receiver.compare("exit") == 0){
-            return 0;
+            return EXIT_SUCCESS;
         }
     }
+    return EXIT_SUCCESS;
 }
