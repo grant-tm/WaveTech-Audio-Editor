547cad445b69590a79e36cd042aac498af04c726
grant-tm <grant.t.morgan@gmail.com>
2023-11-18 11:29:03 -0800

    update services to use new pipe objects

--- source/gui.cpp
+++ source/gui.cpp
@@ -1,6 +1,6 @@
-/*=====================================================================================================================
-== Include Dependencies
-=====================================================================================================================*/
+//=================================================================================================
+// Include Dependencies
+//=================================================================================================
 #include "..\dependencies\AudioFile\AudioFile.h"
 
 //define windows version for asio (suppresses annoying warning)
@@ -15,29 +15,52 @@
 //defines protocols for sending and receiving audio through socket
 #include "communication_protocols.hpp"
 
-/*=====================================================================================================================
-== Standard Libraries
-=====================================================================================================================*/
+//=================================================================================================
+// Standard Libraries
+//=================================================================================================
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <vector>
 
-/*=====================================================================================================================
-== Main
-=====================================================================================================================*/
-int main(int argc, char** argv){
+//=================================================================================================
+// Main
+//=================================================================================================
+int main(int argc, char** argv)
+{
+    // load file 
+    AudioFile<float> file;
+    file.load("../wav_files/sin_sweep.wav");
+    file.printSummary();
     
-    // client-server setup
-    asio::io_context io_context;
-    // resolve endpoint
-    asio::ip::tcp::resolver resolver(io_context);
-    asio::ip::tcp::resolver::results_type endpoint = resolver.resolve("localhost", "daytime");
-    // open socket and connect to endpoint
-    std::shared_ptr<asio::ip::tcp::socket> socket_ptr(new asio::ip::tcp::socket(io_context));
-    asio::connect(*socket_ptr, endpoint);
+    // create a new pipe using port 5500
+    std::unique_ptr<UpstreamPipe> gui_pipe = std::make_unique<UpstreamPipe>(5500);
+    
+    std::cout << "connecting..." << std::endl;
+    gui_pipe->connect(); 
+    std::cout << "connected!" << std::endl;
 
-    //load a file 
+    std::cout << "sending message..." << std::endl;
+    std::cout << gui_pipe->send_message("test") << std::endl;
+    std::cout << "message sent!" << std::endl;
+
+    std::cout << "receiving message..." << std::endl;
+    std::cout << "Message received: " << gui_pipe->recv_message() << std::endl;
+
+    std::cout << "sending audio..." << std::endl;
+    std::cout << gui_pipe->send_audio(&(file.samples)) << std::endl;
+    std::cout << "audio sent!" << std::endl;
+
+    return EXIT_SUCCESS;
+
+    /*
+    // set up connection to dsp
+    asio::io_context io_context;
+    std::shared_ptr<asio::ip::tcp::socket> socket_ptr(new asio::ip::tcp::socket(io_context));
+    asio::ip::tcp::resolver resolver(io_context);
+    asio::connect(*socket_ptr, resolver.resolve("localhost", "5500"));
+
+    // load file 
     AudioFile<float> file;
     file.load("../wav_files/sin_sweep.wav");
     file.printSummary();
@@ -53,9 +76,15 @@ int main(int argc, char** argv){
     buf.resize(2);
     recv_audio(socket_ptr, &buf);
     
-    //put new audio in opened file and save as result.wav 
+    // close connection
+    send_message(socket_ptr, "exit");
+    socket_ptr->close();
+
+    // put new audio in opened file and save as result.wav 
     file.setAudioBuffer(buf);
     file.printSummary();
     file.save("../wav_files/result.wav");
-    return 0;
+    
+    return EXIT_SUCCESS;
+    */
 }
