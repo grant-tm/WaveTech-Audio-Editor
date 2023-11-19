9f82c3d0cede5e37b51ae1b24c6bd7d4d87febe1
grant-tm <grant.t.morgan@gmail.com>
2023-11-19 01:03:15 -0800

    dsp connects gui and reverse

--- source/dsp.cpp
+++ source/dsp.cpp
@@ -14,118 +14,78 @@
 #include <stdlib.h>
 #include <vector>
 //=================================================================================================
-// Connect to reverse microservice
-//=================================================================================================
-void reverse(asio::io_context& io_context, AudioFile<float>::AudioBuffer* buf)
-{
-    /*
-    // connect to reverse microservice
-    std::shared_ptr<asio::ip::tcp::socket> reverse_connection_socket(new asio::ip::tcp::socket(io_context));
-    asio::ip::tcp::resolver resolver(io_context);
-    asio::connect(*reverse_connection_socket, resolver.resolve("localhost", "8800"));
-    
-    // send run command and audio over port 8800
-    send_message(reverse_connection_socket, "run");
-    std::string receiver;
-    recv_message(reverse_connection_socket, &receiver);
-    if(receiver.compare("ready") == 0)
-    {
-        send_audio(reverse_connection_socket, buf);
-    }
-    recv_message(reverse_connection_socket, &receiver);
-    
-    // receive response from reverse
-    send_message(reverse_connection_socket, "ready");
-    recv_audio(reverse_connection_socket, buf);
-
-    return;
-    */
-}
-
-//=================================================================================================
 // Main
 //=================================================================================================
 int main(int argc, char** argv)
 {
-    // create a new pipe using port 5500
-    std::unique_ptr<DownstreamPipe> gui_pipe = std::make_unique<DownstreamPipe>(5500);
-    
-    // wait for connections
-    std::cout << "connecting..." << std::endl;
-    gui_pipe->connect(); 
-    std::cout << "connected!" << std::endl;
-
-    std::cout << "receiving message..." << std::endl;
-    std::cout << "Message received: " << gui_pipe->recv_message() << std::endl;
-
-    std::cout << "sending message..." << std::endl;
-    std::cout << gui_pipe->send_message("test successful") << std::endl;
-    std::cout << "message sent!" << std::endl;
-
-    std::cout << "receiving audio..." << std::endl;
-    std::shared_ptr<AudioFile<float>::AudioBuffer> buffer = gui_pipe->recv_audio();
-    
-    std::cout << "audio received! size: " << (*buffer)[0].size() << std::endl;
-    
-    std::cout << "disconnecting..." << std::endl;
-    gui_pipe->disconnect();
-    std::cout << "disconnected!" << std::endl;
-
-    return EXIT_SUCCESS;
-
-    /*
-    // receive connection from gui
-    asio::io_context io_context;
-    std::shared_ptr<asio::ip::tcp::socket> gui_connection_socket(new asio::ip::tcp::socket(io_context));
-    std::shared_ptr<asio::ip::tcp::acceptor> acceptor_ptr
-        (new asio::ip::tcp::acceptor(io_context, asio::ip::tcp::endpoint(asio::ip::tcp::v4(), 5500)));
-    (*acceptor_ptr).accept(*gui_connection_socket);
 
     while(1)
     {
-        std::cout << "waiting..." << std::endl;
+        // create a new pipe using port 5500
+        std::unique_ptr<DownstreamPipe> gui_service_pipe = std::make_unique<DownstreamPipe>(5500);
+        std::cout << "waiting for connection from gui service" << std::endl;
+        gui_service_pipe->connect(); 
+        std::cout << "connected to gui service" << std::endl;
         
+        std::cout << "waiting for message from gui service" << std::endl;
         std::string receiver;
-        recv_message(gui_connection_socket, &receiver);
-        std::cout << "Message received: " << receiver << std::endl;
-        if(receiver.compare(0, 5, "shift") == 0)
+        receiver = gui_service_pipe->recv_message();
+        std::cout << "message received from gui service: " << receiver << std::endl;
+        
+        if(receiver.compare("shift") == 0)
         {
             std::cout << "shift" << std::endl;
         }
-        else if(receiver.compare(0, 6, "stretch") == 0)
+        else if(receiver.compare("stretch") == 0)
         {
             std::cout << "stretch"<< std::endl;
         }
-        else if(receiver.compare(0, 7, "reverse") == 0)
-        {
-            //create new audio buffer
-            AudioFile<float>::AudioBuffer buf;
-            buf.resize(2);
-            // receive audio from the gui
-            recv_audio(gui_connection_socket, &buf);
-            // send audio to reverse microservice
-            reverse(io_context, &buf);
-            // send reversed audio back to the gui
-            send_audio(gui_connection_socket, &buf);
+        else if(receiver.compare("reverse") == 0)
+        {           
+            std::cout << "disconnecting from gui service" << std::endl;
+            gui_service_pipe->disconnect();
+            std::cout << "disconnected from gui service" << std::endl;
+
+            // create a new pipe using port 8800
+            std::unique_ptr<UpstreamPipe> reverse_service_pipe = 
+                std::make_unique<UpstreamPipe>(8800, gui_service_pipe->get_context());
+           
+            std::cout << "connecting to reverse service" << std::endl;
+            reverse_service_pipe->connect();
+            std::cout << "connected to reverse service" << std::endl;
+            
+            reverse_service_pipe->send_message("run");
+            
+            std::cout << "waiting for response from reverse service" << std::endl;
+            receiver = reverse_service_pipe->recv_message();
+            if(receiver.compare("ready") == 0)
+            {
+                std::cout << "reverse service confirmed responsive" << std::endl;
+                //reverse_service_pipe->send_audio(audio.get());
+            }
+            //gui_service_pipe->send_audio(audio.get());
+            reverse_service_pipe->disconnect();
         }
-        else if(receiver.compare(0, 4, "exit") == 0){
+        else if(receiver.compare(0, 4, "exit") == 0)
+        {
             std::cout << "Exiting..." << std::endl;
             
-            // connect to reverse
-            std::shared_ptr<asio::ip::tcp::socket> reverse_connection_socket(new asio::ip::tcp::socket(io_context));
-            asio::ip::tcp::resolver resolver(io_context);
-            asio::connect(*reverse_connection_socket, resolver.resolve("localhost", "8800"));
-            
+            // create a new pipe using port 8800
+            std::unique_ptr<UpstreamPipe> reverse_service_pipe = 
+                std::make_unique<UpstreamPipe>(8800, gui_service_pipe->get_context());
+           
+            std::cout << "connecting to reverse service" << std::endl;
+            reverse_service_pipe->connect();
+            std::cout << "connected to gui service" << std::endl;
+
             //send run command and audio over port 8800
-            send_message(reverse_connection_socket, "exit");
+            reverse_service_pipe->send_message("exit");
             
             // close connection
-            gui_connection_socket->shutdown(asio::ip::tcp::socket::shutdown_both);
-            gui_connection_socket->close();
+            gui_service_pipe->disconnect();
 
             return EXIT_SUCCESS;
         }
     }
-    */
     return EXIT_SUCCESS;
 }
